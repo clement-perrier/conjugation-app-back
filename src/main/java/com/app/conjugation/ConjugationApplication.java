570473ed70f3bc4642fcec88e280baf37ab9bdbe
Clement Perrier <clement.perrier25@gmail.con>
2024-04-23 19:41:15 +0200

    Global cross origin configuration OK

--- src/main/java/com/app/conjugation/ConjugationApplication.java
+++ src/main/java/com/app/conjugation/ConjugationApplication.java
@@ -3,9 +3,12 @@ package com.app.conjugation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 //@EnableJpaRepositories( basePackages = {"com.app.conjugation.repository"} )
@@ -14,11 +17,16 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class ConjugationApplication {
 	
 	public static void main(String[] args) {
-		/*
-		 * SessionFactory sessionFactory; sessionFactory = new Configuration()
-		 * .configure("hibernate.cfg.xml") .buildSessionFactory();
-		 */
 		SpringApplication.run(ConjugationApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
 }
